4a2e64b97559c034da7a15f82c5bb346f85f5f47
fdramos <fdramos@unillanos.edu.co>
2026-09-04 09:36:47 -0500

    VistaWeb inicializada

---
+++ VistaWeb/src/main/java/org/example/vista/VistaWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.vista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VistaWebApplication.class, args);
+    }
+
+}
---
+++ VistaWeb/src/test/java/org/example/vista/VistaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
